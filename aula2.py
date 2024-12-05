1abf27c4da7b964987fee76f76df64e3f5642889
Tamyy26 <souzatamyris573@gmail.com>
2024-12-05 11:25:58 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,9 @@
+print("Good Morning")
+nome_usuario = "Tamyris" # tipo -> str = string
+idade_usuario = 18 # tipo -> int = integer
+altura_usuario = 1.56 # tipo ->  float = flutuante
+maior_idade = True # tipo -> bool = boolean
+print ("Seja bem vindo" , nome_usuario) 
+print ("idade do usuário" , idade_usuario)
+print ("altura do usuário" , altura_usuario)
+print ("maior de idade" , maior_idade)
